34c01790ee761400ad38ba9ca37f3bab8ccc06d7
Jeff Hui <jeff@jeffhui.net>
2014-03-29 13:58:15 -0700

    Initial Commit

---
+++ InspectorGadget/InspectorGadget.h
@@ -0,0 +1,13 @@
+//
+//  InspectorGadget.h
+//  InspectorGadget
+//
+//  Created by Jeff Hui on 3/29/14.
+//  Copyright (c) 2014 Jeff Hui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface InspectorGadget : NSObject
+
+@end
---
+++ InspectorGadget/InspectorGadget.m
@@ -0,0 +1,13 @@
+//
+//  InspectorGadget.m
+//  InspectorGadget
+//
+//  Created by Jeff Hui on 3/29/14.
+//  Copyright (c) 2014 Jeff Hui. All rights reserved.
+//
+
+#import "InspectorGadget.h"
+
+@implementation InspectorGadget
+
+@end
